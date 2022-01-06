e0eedfe04a02cbd77e6e6d0296edb885475dbae8
ArcticMeteor246 <mats1809@gmail.com>
2022-01-06 20:06:50 +0100

    div

---
+++ Project Mercury/zeroMQ_test.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file zeroMQ_test.cpp
+ * @author ArcticMeteor246 (mats1809@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <opencv2/core.hpp>
