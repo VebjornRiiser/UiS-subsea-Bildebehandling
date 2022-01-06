4671cb9948e5fd51cb5e561369a8fde809c0bc46
ArcticMeteor246 <mats1809@gmail.com>
2022-01-06 18:46:37 +0100

    Ryddet og gjort klart til programmering

---
+++ Project Mercury/mercury.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file mercury.cpp
+ * @author ArcticMeteor246 (mats1809@gmail.com)
+ * @brief Program som håndterer kommunikasjonen mellom forskjellige program som kjører i ROV hjernen
+ * @version 0.1
+ * @date 2022-01-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
