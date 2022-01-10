5d274bfcc83e317b134152f44ae0c570b36023ba
rosaage <tagemellemstrand@hotmail.com>
2022-01-10 15:33:45 +0100

    test, feil i kode

--- Project Mercury/mercury.cpp
+++ Project Mercury/mercury.cpp
@@ -12,7 +12,7 @@
 # include <stdio.h>
 # include <string.h>
 # include <iostream>
-# include <thread>
+//# include <thread>
 
 #ifdef linux //Includes for Linux
 #include <fcntl.h> // Contains file controls like O_RDWR
@@ -20,29 +20,80 @@
 #include <termios.h> // Contains POSIX terminal control definitions
 #include <unistd.h> // write(), read(), close()
 #endif
+#include <unistd.h>
 
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
-#ifdef linux
-int serial_com = uintsd::open("/dev/ttyUSB0", O_RDWR);
-#endif
+
 
 #ifdef _WIN32
+
 #endif
 
-uint8_t serial_melding[13] = {0};
+uint8_t serial_melding[1] = {0};
+uint8_t serialBuffer[11] = {0};
+uint8_t serialBufferTeller = 0;
+int test = 0;
+
+/**
+ * @brief test
+ * 
+ * @param serial_com 
+ */
+void read_serial_port(int serial_com) {
+  read(serial_com, serial_melding, 1);
+}
 
 /**
  * @brief Hovedfunksjon for kommunikasjon
  * 
  * @return int 
  */
-int main( void ){
-  std::cout << "test\n";
-}
+int main( void ) {
+  #ifdef linux
+    std::cout << "Connecting to USB...\n";
+    int serial_com = open("/dev/ttyACM0", O_RDWR);
+  #endif
+  std::cout << serial_com;
+  while (1) {
 
-void read_serial_port(int serial_com){
-  serial_melding = read(serial_com, &buf, 1); 
+    //std::cout << serial_com;
+    //std::cout << "start av loop";
+    if (test = read(serial_com, serial_melding, 1)) {
+
+      std::cout << test;
+      // Start byte
+      if (serial_melding[0] == 0x03) {
+        std::cout << "ok\n";
+        // tøm buffer
+        memset(serialBuffer, 0, 11);
+        serialBufferTeller = 0;
+
+      // Slutt byte
+      } else if (serial_melding[0] == 0x06) {
+        serialBuffer[serialBufferTeller] = serial_melding[0];
+        serialBufferTeller = 0;
+
+        // sett flagg data
+        //std::cout << serialBuffer;
+
+      // Data lenger enn forventa
+      } else if (serialBufferTeller >= 12) {
+        memset(serialBuffer, 0, 11);
+        serialBufferTeller = 0;
+
+      // Behandle data
+      } else {
+        //std::cout << serial_melding[0] << "Writing to buffer \n";
+        serialBuffer[serialBufferTeller] = serial_melding[0];
+        serialBufferTeller++;
+      }
+    } else {
+      std::cout << "Test3";
+    }
+    //std::cout << serial_melding;
+  }
+  std::cout << "test\n";
 }
