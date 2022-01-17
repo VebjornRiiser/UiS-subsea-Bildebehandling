0f506cc570f7f7175656fc43cc175cfcb209e706
christofferuis <christoffer.naess@stud.uis.no>
2022-01-17 07:52:34 +0100

    Change nettwork communication from zmq to normal socket

--- Project Mercury/mercury.py
+++ Project Mercury/mercury.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
+from ast import arg
 from json.tool import main
 from mimetypes import init
 from multiprocessing import context
@@ -9,34 +10,65 @@ from unicodedata import name
 from xml.etree.ElementInclude import include
 import threading
 #import cv2
-import zmq
 import time
-import serial
+#import serial
+import socket
+import json
 
-def mercury():
-    #init
-    context = zmq.Context()
-    network_socket = context.socket(zmq.PAIR)
-    network_socket.connect("tcp://127.0.0.1:6892")
-    print("STARTING SENDING\n")
-    while True:
-        network_socket.send(b'TEST')
-        time.sleep(1)
+dictionary = {"key":[2], "key2": [1]}
+jason_string = json.dumps(dictionary)
+
+#def mercury():
+    # #init
+    # context = zmq.Context()
+    # network_socket = context.socket(zmq.PAIR)
+    # network_socket.connect("tcp://127.0.0.1:6892")
+    # print("STARTING SENDING\n")
+    # while True:
+    #     network_socket.send(b'TEST')
+    #     time.sleep(1)
+
+# Test function for socket connection
+def venus(ip, port):
+    network_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    network_socket.settimeout(1)
+    network_socket.connect((ip, port))
+    for __ in range(10):
+        time.sleep(0.001)
+        try:
+            network_socket.sendall(b"test")
+        except:
+            print("Connection lost")
+            break
+    time.sleep(5)
+    print("test")
+    network_socket.close()
 
 # Reads data from network port
 def network_thread(network_socket, network_callback, flag):
-    network_socket.bind("tcp://127.0.0.1:6892")
+    #network_socket.bind("tcp://127.0.0.1:6892")
     print("Server started\n")
     while flag[0]:
-        melding = network_socket.recv()
-        network_callback(melding)
+        try:
+            melding = network_socket.recv(1024)
+            if melding == b"END":
+                break
+            else:
+                network_callback(melding)
+        except:
+            print("Exception")
+            break
+    network_socket.close()
     print(f'Thread stopped')
 
 # Reads serial data
 def USB_thread(h_serial, USB_callback, flag):
     while flag[1]:
-        melding = h_serial.readline().decode("utf8").strip("\n")
-        USB_callback(melding)
+        try:
+            melding = h_serial.readline().decode("utf8").strip("\n")
+            USB_callback(melding)
+        except:
+            pass
     print("USB thread stopped")
 
 class Callbacks:
@@ -45,12 +77,14 @@ class Callbacks:
         self.network_status = False
         self.USB_status = False
         self.status_flag_list = [1,1,1,1,1] # Index 0 = Network, Index 1 = USB, Index 3 = ?, index 4 = ?
-        self.connect_ip = "192.168.137.2"
-        self.connect_port = "6899"
+        self.connect_ip = "127.0.0.1"
+        self.connect_port = 6899
+        self.net_init()
+        
 
         # Network socket recive
-        context = zmq.Context()
-        self.network_rcv_socket = context.socket(zmq.PAIR)
+        #context = zmq.Context()
+        #self.network_rcv_socket = context.socket(zmq.PAIR)
 
         # Network socket send
         #context2 = zmq.Context()
@@ -64,9 +98,18 @@ class Callbacks:
         #self.toggle_USB()
         #self.network_snd_socket.send_string(f'USB connection started')
 
+
+    def net_init(self):
+        self.network_rcv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.network_rcv_socket.bind((self.connect_ip, self.connect_port))
+        self.network_rcv_socket.settimeout(1)
+        self.network_rcv_socket.listen()
+        self.network_connection, self.network_address = self.network_rcv_socket.accept()
+
+
     def network_callback(self, message):
-        self.serial.write(message)
-        pass
+        #self.serial.write(message)
+        print(message)
 
     def toggle_network(self):
         if self.network_status:
@@ -74,7 +117,7 @@ class Callbacks:
             self.network_status = False
         else:
             print("Starting thread")
-            self.network_trad = threading.Thread(name="Network_thread",target=network_thread, daemon=True, args=(self.network_rcv_socket, self.network_callback, self.status_flag_list))
+            self.network_trad = threading.Thread(name="Network_thread",target=network_thread, daemon=True, args=(self.network_connection, self.network_callback, self.status_flag_list))
             self.network_trad.start()
             self.network_status = True
 
@@ -97,9 +140,13 @@ class Callbacks:
 
 
 if __name__ == "__main__":
+    ip = "127.0.0.1"
+    port = 6899
+    venus_trad = threading.Thread(name="venus", target=venus, args=(ip, port))
+    venus_trad.start()
     a = Callbacks()
     a.toggle_network()
-    time.sleep(3)
-    mercury()
-    while True:
+    print("For loop started")
+    for __ in range(10):
         time.sleep(1)
+    print("For loop stopped")
