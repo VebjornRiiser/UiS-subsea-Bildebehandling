4c1553e2645fa9169c85dde8ffbaca262f7a305f
christofferuis <christoffer.naess@stud.uis.no>
2022-02-14 19:17:29 +0100

    Camera is now a class, added functions to calculate distance and size of objects.

--- python/distance.py
+++ python/distance.py
@@ -1,2 +1,117 @@
+from operator import pos
 import cv2
+from sys import platform
+import numpy as np
+import time
 
+# TESTSCRIPT FOR AVSTANDSMÅLING
+
+def white_balance(img):
+    result = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result[:, :, 2] = result[:, :, 2] - ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+    return result
+
+
+def camera(camera_id):
+    print("Camera Thread started")
+    shared_list = [1, 0, 0, 0]
+    picture = np.array
+    if platform == "linux" or platform == "linux2":
+        feed = cv2.VideoCapture(camera_id, cv2.CAP_V4L2)
+    else:
+        feed = cv2.VideoCapture(camera_id, cv2.CAP_SHOW)
+    feed.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+    frame_width = 2560
+    frame_height = 720
+    feed.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+    feed.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+    frame_height = int(feed.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_width = int(feed.get(cv2.CAP_PROP_FRAME_WIDTH))
+    crop_width = int(frame_width/2)
+    feed.set(cv2.CAP_PROP_FPS, 30)
+    feed.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+    print(crop_width)
+    print(frame_width)
+    print(frame_height)
+    run = True
+    feed.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+    feed.set(cv2.CAP_PROP_EXPOSURE, 600)
+    if not (feed.isOpened()):
+        print("Could not open video device")
+        run = False
+    total_list = []
+    while run:
+        ref, frame = feed.read()
+        crop_frame = frame[:frame_height, :crop_width]
+        crop_frame2 = frame[:frame_height,crop_width:]
+        crop_frame = white_balance(crop_frame)
+        crop_frame2 = white_balance(crop_frame2)
+        crop_frame, cord, s1 = contour_img(crop_frame)
+        crop_frame2, cord2, s2 = contour_img(crop_frame2)
+        if len(cord) == 2 and len(cord2)==2:
+            distance = calc_distance([cord, cord2], 60 , 6)
+            if distance > 10:
+                cv2.putText(crop_frame, f'Distance:{distance} cm',cord, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3, cv2.LINE_AA)
+                cv2.putText(crop_frame, f'Distance:{distance} cm',cord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+                width  = calc_size (s1[0],(cord,cord2) ,  0)
+                cv2.putText(crop_frame, f'Width:{width} cm',(cord[0], cord[1]+100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        cv2.imshow("Named Frame",crop_frame)
+        cv2.imshow("named Frame2", crop_frame2)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    feed.release()
+    cv2.destroyAllWindows()
+
+
+def contour_img(image):
+    cvt_pic = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    color_lower = np.array([60,100,50])
+    color_upper = np.array([160,255,255])
+    mask = cv2.inRange(cvt_pic, color_lower, color_upper)
+    cv2.erode(mask,np.ones((5,5),np.uint8), mask)
+    ret, threash, = cv2.threshold(mask, 80, 255, 1)
+    #cv2.imshow("name", threash)
+    cont, hie = cv2.findContours(threash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    ny_cont = []
+    liste_paa_Sizes = list(map(cv2.contourArea , cont))
+    for index, areal in enumerate(liste_paa_Sizes):
+        if areal > 5000 and areal < 500000:
+            ny_cont.append(cont[index])
+    rect = []
+    for a in ny_cont:
+        rect.append(cv2.minAreaRect(a))
+    if len(rect) > 0:
+        box = np.int0(cv2.boxPoints(rect[0]))
+        cv2.drawContours(image, [box] , -1, (0, 0, 0), 2 )
+        return image, [int(i) for i in rect[0][0]], [int(i) for i in rect[0][1]]
+    else:
+        return image, rect, rect
+
+
+def get_center(contur):
+    center = cv2.moments(contur)
+    x = int(center["m10"] / center["m00"])
+    y = int(center["m01"] / center["m00"])
+    return (x,y)
+
+
+def calc_distance(centers, focal_len, camera_space):
+    dist = abs(centers[0][0]-centers[1][0])
+    return int(((focal_len*camera_space)/dist)*100)
+
+
+def calc_size(num_pixels:int, centers, axis:int=0):
+    """Calculates size with center points and number of pixelse size of object, axis=0 refers to horisontal measurment"""
+    factor = -0.002344
+    constant = 0.541
+    dist = abs(centers[0][0]-centers[1][0])
+    #print(f'Distance between pic: {dist}, Object size: {num_pixels}')
+    return round((dist * factor + constant)*num_pixels, 1)
+
+
+if __name__ == "__main__":
+    camera(2)
--- python/theia.py
+++ python/theia.py
@@ -1,4 +1,3 @@
-from ast import Return
 import threading, mjpeg_stream, cv2, time, math
 from socket import AF_INET, SOCK_DGRAM, socket
 import numpy as np
@@ -7,15 +6,7 @@ from subprocess import Popen, PIPE
 import time
 from sys import platform
 import pickle as p
-
-
-
-def contour_img(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, threash, = cv2.threshold(gray, 127, 255, 0)
-    cont, hie = cv2.findContours(threash, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(image, cont, -1, (0, 0, 0), 3 )
-    return image
+from distance import contour_img, calc_size, calc_distance
 
 
 def udp_picture_transfer(pipe_recive, port):
@@ -42,6 +33,95 @@ def udp_picture_transfer(pipe_recive, port):
             time.sleep(0.001)
 
 
+
+def white_balance(img):
+    result = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result[:, :, 2] = result[:, :, 2] - ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result = cv2.cvtColor(result, cv2.COLOR_LAB2BGR)
+    return result
+
+class Camera():
+    def __init__(self, id:int, width:int=2560, height:int=720, framerate:int=30 ) -> None:
+        self.id = id
+        self.height = height
+        self.width = width
+        if platform == "linux" or platform == "linux2":
+            self.feed = cv2.VideoCapture(self.id, cv2.CAP_V4L2)
+        else:
+            self.feed = cv2.VideoCapture(self.id, cv2.CAP_SHOW)
+        self.set_picture_size(self.width, self.height)
+        self.feed.set(cv2.CAP_PROP_FPS, framerate)
+        self.feed.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+        self.feed.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0)
+        self.feed.set(cv2.CAP_PROP_EXPOSURE, 600)
+        self.feed.set(cv2.CAP_PROP_AUTO_WB, 1)
+        print(self.feed.get(cv2.CAP_PROP_AUTO_WB))
+
+
+    def set_picture_size(self, width:int=2560, height:int=720):
+        self.feed.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        self.feed.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.feed.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        self.height = int(self.feed.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.width = int(self.feed.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.crop_width = int(self.width/2)
+
+    def aq_image(self, double:bool=False):
+        ref, frame = self.feed.read()
+        crop = frame[:self.height, :self.crop_width]
+        if double:
+            crop2 = frame[:self.height,self.crop_width:]
+            crop2 = white_balance(crop2)
+            crop = white_balance(crop)
+            return crop, crop2
+        else:
+            crop = white_balance(crop)
+            return crop
+
+
+def find_calc_shapes(pic1, pic2):
+    pic, cord, s1 = contour_img(pic1)
+    pic2, cord2, s2 = contour_img(pic2)
+    if len(cord) == 2 and len(cord2) == 2:
+        distance = calc_distance([cord, cord2], 60, 6)
+        if distance > 10:
+            cv2.putText(pic, f'Distance:{distance} cm',cord, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3, cv2.LINE_AA)
+            cv2.putText(pic, f'Distance:{distance} cm',cord, cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+            width  = calc_size (s1[0],(cord,cord2) ,  0)
+            cv2.putText(pic, f'Width:{width} cm',(cord[0], cord[1]+100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+    return pic1
+
+
+def camera_thread(camera_id, connection, picture_send_pipe):
+    print('Camera:{camera_id} started')
+    cam = Camera(camera_id)
+    shared_list = [1, 0, 0, 0]
+    threading.Thread(name="Camera_con", target=pipe_com, daemon=True, args=(connection, None, None, shared_list)).start()
+    run = True
+    video_feed = False
+    if not (cam.feed.isOpened()):
+        print('Could not open video device')
+        run = False
+    while run:
+        if shared_list[1] == 0:
+            pic = cam.aq_image()
+        elif shared_list[1] == 1:
+            pic, pic2 = cam.aq_image(True)
+            pic = find_calc_shapes(pic, pic2)
+        if video_feed:
+            picture_send_pipe.send(pic)
+        else:
+            cv2.imshow("Named Frame",pic)
+            #cv2.imshow("Named2", pic2)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cam.feed.release()
+    cv2.destroyAllWindows()
+        
+
 #TODO click funksjon, Show image for debug/test 
 def camera(camera_id, connection, picture_send_pipe):
     print("Camera Thread started")
@@ -62,9 +142,6 @@ def camera(camera_id, connection, picture_send_pipe):
     crop_width = int(frame_width/2)
     feed.set(cv2.CAP_PROP_FPS, 30)
     feed.set(cv2.CAP_PROP_AUTOFOCUS, 1)
-    print(crop_width)
-    print(frame_width)
-    print(frame_height)
     run = True
     f_video_feed = True
     if not (feed.isOpened()):
@@ -80,7 +157,7 @@ def camera(camera_id, connection, picture_send_pipe):
             shared_list[0] = 1
         ref, frame = feed.read()
         crop_frame = frame[:frame_height, :crop_width]
-        crop_frame = contour_img(crop_frame)
+        #crop_frame = contour_img(crop_frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         if f_video_feed:
@@ -97,8 +174,8 @@ def pipe_com(connection, callback=None, name=None, list=None):
             callback(msg, name)
     else:
         while list[0]:
-            list[1] = print(connection.recv())
-            list[0] = 0
+            list[1] = connection.recv()
+            print(f"{list[1] = }")
 
 
 #TODO cli_runtime
@@ -126,15 +203,13 @@ class Theia():
         self.cam_front_id = self.find_cam("3-2") # Finner kamera på usb
         self.cam_back_id = self.find_cam("4-2")
         if not self.cam_front_id:
-            self.camera_status['front'] = 0
+            self.camera_status['front'][1] = 0
         else:
-            self.camera_status['front'] = 1
+            self.camera_status['front'][1] = 1
         if not self.cam_back_id:
-            self.camera_status['back'] = 0
+            self.camera_status['back'][1] = 0
         else:
-            self.camera_status['back'] = 1
-        
-            
+            self.camera_status['back'][1] = 1
 
     def find_cam(self, cam):
         cmd = ["/usr/bin/v4l2-ctl", "--list-devices"]
@@ -146,32 +221,32 @@ class Theia():
         return False
 
     def toggle_front(self, cam_id: int=0):
-        if self.camera_status['front'][1]:
+        if self.camera_status['front'][0] == 1:
             self.front_camera_prosess.kill()
-            self.camera_status['front'][1] = 0
+            self.camera_status['front'][0] = 0
         else:
-            if self.camera_status['front'][0]:
-                self.host_cam1, self.client_cam1 = Pipe()
+            if self.camera_status['front'][1]:
+                self.host_cam_front, self.client_cam1 = Pipe()
                 send_front_pic, recive_front_pic = Pipe()
-                self.front_camera_prosess = Process(target=camera, daemon=True, args=(self.cam_front_id, self.client_cam1, send_front_pic)).start()
-                self.front_cam_com_thread = threading.Thread(name="COM_cam_1",target=pipe_com, daemon=True, args=(self.host_cam1, self.camera_com_callback, self.cam_front_name)).start()
+                self.front_camera_prosess = Process(target=camera_thread, daemon=True, args=(self.cam_front_id, self.client_cam1, send_front_pic)).start()
+                self.front_cam_com_thread = threading.Thread(name="COM_cam_1",target=pipe_com, daemon=True, args=(self.host_cam_front, self.camera_com_callback, self.cam_front_name)).start()
                 self.steam_video_prosess = Process(target=mjpeg_stream.run_mjpeg_stream, daemon=True, args=(recive_front_pic, self.port_camfront_feed)).start()
                 #self.steam_video_prosess = Process(target=udp_picture_transfer, daemon=True, args=(recive_front_pic, self.port_camfront_feed)).start()
-                self.camera_status['front'][1] = 1
+                self.camera_status['front'] = 1
                 return True
             else:
                 #! TODO Send message to topside, camera could not be found
                 return False
 
     def toggle_back(self, cam_id: int=2):
-        if self.camera_status['back'][1]:
+        if self.camera_status['back'][1] == 1:
             self.back_camera_prosess.kill()
             self.camera_status['back'][1] = 0
         else:
             if self.camera_status['back'][0]:
                 self.host_cam2, self.client_cam2 = Pipe()
                 send_back_pic, recive_back_pic = Pipe()
-                self.back_camera_prosess = Process(target=camera, daemon=True, args=(self.cam_back_id, self.client_cam2, send_back_pic)).start()
+                self.back_camera_prosess = Process(target=camera_thread, daemon=True, args=(self.cam_back_id, self.client_cam2, send_back_pic)).start()
                 self.front_cam_com_thread = threading.Thread(name="COM_cam_2",target=pipe_com, daemon=True, args=(self.host_cam2, self.camera_com_callback, self.cam_front_name)).start()
                 self.steam_video_prosess = Process(target=mjpeg_stream.run_mjpeg_stream, daemon=True, args=(recive_back_pic, self.port_camback_feed)).start()
                 #self.steam_video_prosess = Process(target=udp_picture_transfer, daemon=True, args=(recive_back_pic, self.port_camback_feed)).start()
@@ -205,7 +280,14 @@ class Theia():
 if __name__ == "__main__":
     print("Main=Theia")
     s = Theia()
-    s.toggle_front()
-    s.toggle_back()
+    s.camera_status['front'][1] = 1
+    s.cam_front_id = 2
+    s.toggle_front(2)
+    
+    #s.toggle_back()
     for __ in range(9999999):
         time.sleep(1)
+        time.sleep(5)
+        s.host_cam_front.send(0)
+        time.sleep(10)
+        s.host_cam_front.send(1)
