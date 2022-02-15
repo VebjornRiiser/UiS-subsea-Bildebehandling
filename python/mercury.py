e490b4f1c1554bbbec0632e3db4897416ab789e5
rosaage <tagemellemstrand@hotmail.com>
2022-02-15 14:44:12 +0100

    .

--- python/mercury.py
+++ python/mercury.py
@@ -80,7 +80,7 @@ def network_thread(network_handler, network_callback, flag):
                 continue
             else:
                 #print(melding)
-                print('før network callback')
+                #print('før network callback')
                 network_callback(melding)
         except ValueError as e:
             print(f'Feilkode i network thread feilmelding: {e}\n\t{melding = }')
@@ -168,7 +168,7 @@ class Mercury:
     def network_callback(self, data: bytes) -> None:
         data:str = bytes.decode(data, "utf-8")
         for message in data.split( json.dumps("*") ):
-            print(f'Sjekker for heartbeat {data = }, {message = }')
+            #print(f'Sjekker for heartbeat {data = }, {message = }')
             if message == json.dumps('heartbeat') or message == "":
                 continue
             else:
