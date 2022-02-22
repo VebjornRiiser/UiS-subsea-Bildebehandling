971a7c5fbb91b34257d919f173141fe19ebae07f
Christoffer <c.ness@stud.uis.no>
2022-02-22 14:34:53 +0100

    rotate camera

--- python/theia.py
+++ python/theia.py
@@ -74,6 +74,7 @@ class Camera():
 
     def aq_image(self, double:bool=False):
         ref, frame = self.feed.read()
+        frame = cv2.rotate(frame, cv2.cv2.ROTATE_180_CLOCKWISE)
         crop = frame[:self.height, :self.crop_width]
         if double:
             crop2 = frame[:self.height,self.crop_width:]
