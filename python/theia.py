a8104988fcd5a3109072377932cfbc745008c670
Christoffer <c.ness@stud.uis.no>
2022-03-11 08:11:54 +0100

    asd

--- python/theia.py
+++ python/theia.py
@@ -303,7 +303,7 @@ class Theia():
         self.cam_front_id = self.find_cam(".7") # Checks if a camera is connected on this port
         self.cam_back_id = self.find_cam("3-2")
         for a in range(10):
-            if self.find_cam(f'3-{a}'):
+            if self.find_cam(f'.{a}'):
                 print(a)
         if not self.cam_front_id:
             print(f'Did no find front camera')
