4672089c1cd8096cc176f3f75d816892a64c1451
Christoffer <c.ness@stud.uis.no>
2022-03-11 08:29:26 +0100

    asd

--- python/theia.py
+++ python/theia.py
@@ -319,9 +319,9 @@ class Theia():
 
     def find_cam(self, cam):
         cmd = ["/usr/bin/v4l2-ctl", "--list-devices"]
+        print(cmd)
         out, err = Popen(cmd, stdout=PIPE, stderr=PIPE).communicate()
         out, err = out.strip(), err.strip()
-        print(out)
         for l in [i.split("\n\t") for i in out.decode("utf-8").split("\n\n")]:
             if cam in l[0]:
                 return l[1]
