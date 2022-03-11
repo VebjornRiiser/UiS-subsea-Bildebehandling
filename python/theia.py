31507f1ca381118683fb759a26644b54bf06a0be
Christoffer <c.ness@stud.uis.no>
2022-03-11 11:18:24 +0100

    asd

--- python/theia.py
+++ python/theia.py
@@ -190,6 +190,7 @@ def camera_thread(camera_id, connection, picture_send_pipe, picture_IA_pipe):
                     cam.feed.release()
                     cv2.destroyAllWindows()
                     break
+                mode = int(mode)
                 print(f'Mode set to{mode}')
         if mode == 0:
             pic = cam.aq_image()
