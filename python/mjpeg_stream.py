5a403c38ea1a9cacdac0b8ed19179033117edf88
Christoffer <c.ness@stud.uis.no>
2022-03-14 17:12:38 +0100

    asd

--- python/mjpeg_stream.py
+++ python/mjpeg_stream.py
@@ -38,7 +38,7 @@ class CamHandler(BaseHTTPRequestHandler):
                             print('Starting video file creation!\n')
                             self.video_cap ^= True
                             if self.video_cap:
-                                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                                fourcc = cv2.VideoWriter_fourcc(*'MJPG')
                                 self.video = cv2.VideoWriter(f'vid_{time.asctime()}.mp4', fourcc, 30.0, (1280, 720))
                             else:
                                 self.video.release()
