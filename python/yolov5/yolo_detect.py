c685b7aec67ab94a12a2873282e1a17ffe4c319d
christofferuis <christoffer.naess@stud.uis.no>
2022-03-17 15:42:39 +0100

    Find fishin using yolo works, missing draw

--- python/yolov5/yolo_detect.py
+++ python/yolov5/yolo_detect.py
@@ -4,13 +4,14 @@ from sys import platform
 import numpy as np
 import time
 import torch
+from zmq import device
 from models.common import DetectMultiBackend
 from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
 from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
-
+from utils.augmentations import letterbox
 
 
 class Yolo():
@@ -18,22 +19,34 @@ class Yolo():
         self.device = select_device('')
         self.weights = 'models/best.pt'
         self.data = 'data/coco128.yaml'
-        self.conf_trees = 0.25
+        self.conf_trees = 0.60
         self.iou_tres = 0.45
         self.model = DetectMultiBackend(self.weights, self.device, False, self.data,)
-        #self.model.warmup(imgsz=(1, 2, 640, 640), half=False) # Tage u fix, ikkje sikker ka me sga her
+        imgsz=(640, 640)
+        self.imgsz = check_img_size(imgsz, s=self.model.stride)
+        tride, names, pt, jit, onnx, engine = self.model.stride, self.model.names, self.model.pt, self.model.jit, self.model.onnx, self.model.engine
+        if pt or jit:
+            self.model.model.float()
+        self.model.warmup(imgsz=(1, 3, *imgsz), half=False) # Tage u fix, ikkje sikker ka me sga her
+        
 
     def yolo_image(self, image, test = False): #Find shapes using YOLO (Mostly fish)
+        gn = torch.tensor(image.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        image = letterbox(image, self.imgsz, stride=self.model.stride, auto=True)[0]
+        image = image.transpose((2, 0, 1))[::-1]
+        image = np.ascontiguousarray(image)
         image = torch.from_numpy(image).to(self.device)
         image = image.float()
         image /= 255
         if len(image.shape) == 3: # What this does it not clear
             image = image[None]
-        pred = self.model(image, False, False)
+        pred = self.model(image, augment=False, visualize=False)
         pred = non_max_suppression(pred, self.conf_trees)
         for i, detected in enumerate(pred):
-            print("Deteced object\n")
-            print(detected)
+            for *xyxy, conf, cls in reversed(detected):
+                #print(f'{float(conf)}\n')
+                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
+                print(xywh)
         if test:
             cv2.imshow('WindowName', image)
 
