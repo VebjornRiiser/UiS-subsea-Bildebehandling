f69effd4dd011286fa1250cbc4f7e1b380fa66eb
ArcticMeteor246 <mats1809@gmail.com>
2022-03-18 13:22:08 +0100

    Merge branch 'main' into Autonomkjøring

--- python/autonomkjøring.py
+++ python/autonomkjøring.py
@@ -16,14 +16,39 @@ class Autonom:
     
     def dock(self) -> None:
         self.logger.info("Autonom docking startet")
+        # STARTUP kode for docking -- En form for kalibrering og info til operatør om hva som skjer og en bekreftelse av noe slag
         
+        # Henter bilde
+        # PREPROSSES bilder
+        # VP kode for docking -- Posisjon osv
+        # HENT regulator verdier med data fra VP kode
+        # SEND kjøredata til CAN (hermes)
+        # SEND bilde og evt data opp til TOPSIDE
         
     def merd(self) -> None:
         self.logger.info("Autonom merd skanning startet")
-    
+        # STARTUP kode for merd inspekson -- En form for kalibrering og info til operatør om hva som skjer og en bekreftelse av noe slag
+        
+        # Henter bilde
+        # PREPROSSES bilder
+        # VP kode for merd inspeksjon -- Leter etter hull i merden (blob detection med et gitt areal, oppgaven oppgir minimums og maksimums), kunne vise hvor den tror at hullet er og lagre info om hull (ID, størrelse og posisjon)
+        # HENT regulator verdier med data fra VP kode
+        # SEND kjøredata til CAN (hermes)
+        # SEND bilde og evt data opp til TOPSIDE        
+        
+        
     def mosaic(self) -> None:
         self.logger.info("Autonom mosaic startet")
-    
+        # STARTUP kode for mosaic -- En form for kalibrering og info til operatør om hva som skjer og en bekreftelse av noe slag
+        
+        # Henter bilde -- gir bilde en ID og en plass i rutenettet som det skal plasseres i
+        # VP kode for navigasjon -- Navigere seg etter etter rutenettet
+        # HENT regulator verdier med data fra VP kode
+        # SEND kjøredata til CAN (hermes)
+        # SEND bilde og evt data opp til topside
+        
+        # VP kode for mosaic -- utfører sammenslåing av bilde som er blitt tatt, med utgangspunkt i ID og rutenett posisjon
+
     
     
 if __name__ == "__main__":
