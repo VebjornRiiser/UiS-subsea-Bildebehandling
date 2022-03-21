9ca532b5f38279bee1981c152cafdb595378f763
rosaage <tagemellemstrand@hotmail.com>
2022-03-21 13:35:07 +0100

    test bypass av kamera

--- python/theia.py
+++ python/theia.py
@@ -409,17 +409,13 @@ class Theia():
         self.cam_back_name =  'tage'
         self.set_front_zero = [200, {"tilt": 0}]
         self.set_back_zero = [201, {"tilt": 0}]
-        #self.check_hw_id_cam()
-        # Test med virtual camera frå obs
-        self.cam_front_id = "4"
-        self.cam_back_id = "7"
-        self.camera_status['front'][1] = 1
-        self.camera_status['back'][1] = 1
-        # Test med virtual camera frå obs
+        self.check_hw_id_cam()
 
     def check_hw_id_cam(self):
-        self.cam_front_id = self.find_cam(".7") # Checks if a camera is connected on this port
-        self.cam_back_id = self.find_cam("3-2")
+        #self.cam_front_id = self.find_cam(".7") # Checks if a camera is connected on this port
+        #self.cam_back_id = self.find_cam("3-2")
+        self.cam_front_id = self.find_cam("004")
+        self.cam_back_id = self.find_cam("007")
         if not self.cam_front_id:
             print(f'Did no find front camera')
             self.camera_status['front'][1] = 0
