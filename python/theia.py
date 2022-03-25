99f968ee956aa9d0a8b0f87e1e619542667753de
Christoffer <c.ness@stud.uis.no>
2022-03-25 10:06:42 +0100

    Commented out test code in theia

--- python/theia.py
+++ python/theia.py
@@ -412,10 +412,10 @@ class Theia():
         self.check_hw_id_cam()
 
     def check_hw_id_cam(self):
-        #self.cam_front_id = self.find_cam(".7") # Checks if a camera is connected on this port
-        #self.cam_back_id = self.find_cam("3-2")
-        self.cam_front_id = self.find_cam("004")
-        self.cam_back_id = self.find_cam("007")
+        self.cam_front_id = self.find_cam(".7") # Checks if a camera is connected on this port
+        self.cam_back_id = self.find_cam("3-2")
+        #self.cam_front_id = self.find_cam("004")
+        #self.cam_back_id = self.find_cam("007")
         if not self.cam_front_id:
             print(f'Did no find front camera')
             self.camera_status['front'][1] = 0
