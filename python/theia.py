b0f141cc3bce709ed2b7b449f31fe31b4e53f5bd
Christoffer <c.ness@stud.uis.no>
2022-03-25 12:13:50 +0100

    asd

--- python/theia.py
+++ python/theia.py
@@ -240,7 +240,7 @@ def image_aqusition_thread(connection, boli):
         else:
             if mode == 1:
                 if len(mess) == 2:
-                    #res1 = yal.yolo_image(mess[0]) # Result from left cam
+                    res1 = yal.yolo_image(mess[0]) # Result from left cam
                     #res2 = yal.yolo_image(mess[1]) # Result from right cam
                     #mached_list = find_calc_shapes(mess[0], mess[1])
                     mached_list = []
