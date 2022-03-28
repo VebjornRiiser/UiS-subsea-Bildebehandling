bbe008d6adaf1492a002c0672b353edababb250e
Christoffer <c.ness@stud.uis.no>
2022-03-28 14:31:27 +0200

    asd

--- python/theia.py
+++ python/theia.py
@@ -267,7 +267,8 @@ def draw_on_img(pic, frames):
         cv2.putText(pic, item.name, (item.rectangle[0][0], item.rectangle[0][1]+40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 3)
         cv2.putText(pic, item.name, (item.rectangle[0][0], item.rectangle[0][1]+40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 1)
         if item.dept != 0:
-            cv2.putText(pic, f'Distance:{item.dept} cm',item.position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3, cv2.LINE_AA)
+            print(f'Pos:{item.position}')
+            #cv2.putText(pic, f'Distance:{item.dept} cm',item.position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3, cv2.LINE_AA)
             #cv2.putText(pic, f'Distance:{item.dept} cm',item.position, cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
         #cv2.putText(pic, f'Distance:{a.dept} cm',a.position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 3, cv2.LINE_AA)
         #cv2.putText(pic, f'Distance:{a.dept} cm',a.position, cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
