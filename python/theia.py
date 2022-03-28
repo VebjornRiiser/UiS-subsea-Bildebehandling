fd0382dd8fcd1ce2e7a5815b34746b8b8fe264b3
Christoffer <c.ness@stud.uis.no>
2022-03-28 11:59:42 +0200

    asd

--- python/theia.py
+++ python/theia.py
@@ -219,7 +219,7 @@ def find_calc_shapes(pic1, pic2):
 def find_same_objects(obj_list1:list, obj_list2:list):
     checked_object_list = []
     print("Found fish")
-    for obj1 in enumerate(obj_list1):
+    for obj1 in obj_list1:
         for obj2 in obj_list2:
             if obj1.position[1] == obj2.position[1]:
                 print(f'Found same object in both pictures')
