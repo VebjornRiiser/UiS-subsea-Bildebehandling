a1ae23078ed41a4a3bcaa398e973019f348d7c44
Christoffer <c.ness@stud.uis.no>
2022-03-31 14:55:01 +0200

    asd

--- python/theia.py
+++ python/theia.py
@@ -271,7 +271,7 @@ class Athena():
                         mached_pixels = sorted(mached_pixels, key = lambda x:x.distance)
                         new_list = []
                         for a in mached_pixels:
-                            if a.distance < 30:
+                            if a.distance < 70:
                                 new_list.append(a)
                     except Exception as i:
                         print(i)
