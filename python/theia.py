bfa9a247ade643956987d8014a717facaeb679df
Christoffer <c.ness@stud.uis.no>
2022-04-01 13:51:27 +0200

    asd

--- python/theia.py
+++ python/theia.py
@@ -347,10 +347,9 @@ class Athena():
                                     crop1 = cv2.circle(crop1, (int(kp1[a.queryIdx].pt[0]), int(kp1[a.queryIdx].pt[1])), 4, (255,0,0), -1)
                                     crop2 = cv2.circle(crop2, (int(kp2[a.trainIdx].pt[0]), int(kp2[a.trainIdx].pt[1])) , 4, (255,0,0), -1)
                                     dif_list.append(abs(kp1[a.queryIdx].pt[0] - kp2[a.trainIdx].pt[0]+offset))
-                                    print(f'Disparity: {kp1[a.queryIdx].pt[0] - kp2[a.trainIdx].pt[0]+offset}')
                             if len(dif_list) > 2:
                                 obj1.dept = calc_distance(statistics.median(dif_list))
-                                print(f'Distance: {obj1.dept}')
+                                print(f'Disparity: {statistics.median(dif_list)}')
                                 #cv2.imshow("TAGE1!!!!", crop1)
                                 #cv2.imshow("TAGE2!!!!", crop2)
                                 #if cv2.waitKey(1) & 0xFF == ord('q'):
