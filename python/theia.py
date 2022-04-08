1fc3be339a0541de9144bd10dca0a6febbfed302
Christoffer <c.ness@stud.uis.no>
2022-04-08 09:51:04 +0200

    asd

--- python/theia.py
+++ python/theia.py
@@ -372,10 +372,10 @@ class Athena():
                             if len(dif_list) > 2:
                                 obj1.dept = calc_distance(statistics.median(dif_list))
                                 print(f'Disparity: {statistics.median(dif_list)}')
-                                cv2.imshow("TAGE1!!!!", crop1)
-                                cv2.imshow("TAGE2!!!!", crop2)
-                                if cv2.waitKey(1) & 0xFF == ord('q'):
-                                    break
+                                #cv2.imshow("TAGE1!!!!", crop1)
+                                #cv2.imshow("TAGE2!!!!", crop2)
+                                #if cv2.waitKey(1) & 0xFF == ord('q'):
+                                #    break
                                 #pass
                                 #print(f'Mean:{statistics.mean(dif_list)}')
                                 #print(f'Median:{statistics.median(dif_list)}')
