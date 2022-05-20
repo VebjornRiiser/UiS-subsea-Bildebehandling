a1b8deedc9561bcd1fa6ac3aff0b18540714e11b
Christoffer <c.ness@stud.uis.no>
2022-05-20 12:18:17 +0200

    asd

--- python/theia.py
+++ python/theia.py
@@ -253,7 +253,7 @@ class Camera():
         cv2.rectangle(pic, self.squarestart, self.squarestop, self.color, 2)
         cv2.putText(pic, f'{dept}', (int(self.center[0]-self.length-10), int(self.center[1])), cv2.FONT_HERSHEY_SIMPLEX, 1, self.color, 2)
         for a in range(0 , 100 , 10):
-            cv2.line(pic, (int(self.center[0]-self.length), int(self.center[1]+a-50)), (int(self.center[0]-self.length+20), int(self.center[1]+a-50)), self.color, 2)
+            cv2.line(pic, (int(self.center[0]-self.length-48), int(self.center[1]+a-50)), (int(self.center[0]-self.length-22), int(self.center[1]+a-50)), self.color, 2)
         points = np.array(self.cursor.get_points(self.sensor['gyro'][1]))
         cv2.polylines(pic, [points], False, (0,0,255), 2)
 
